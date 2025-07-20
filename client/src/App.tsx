773c9f5614dcfdd3c50088bd899ab978ece241e7
rajatsaini2003 <rajatsaini9460@gmail.com>
2025-07-20 19:50:26 +0530

    FrontEnd Setup : React + TS + Tailwind

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      hi this is chat app
+      <button className='bg-red-300 '>Hello tailwind check</button>
+    </>
+  )
+}
+
+export default App
